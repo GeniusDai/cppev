37193076fd3ec1792211b8229c268674087d31b9
GeniusDai <1972789815@qq.com>
2022-07-22 22:15:35 +0800

    add posix shared_memory and semaphore

---
+++ src/include/cppev/ipc.h
@@ -0,0 +1,70 @@
+#ifndef _ipc_h_6C0224787A17_
+#define _ipc_h_6C0224787A17_
+
+#include <string>
+#include <semaphore.h>
+#include <sys/mman.h>
+#include "cppev/common_utils.h"
+
+namespace cppev
+{
+
+class shared_memory
+: public uncopyable
+{
+public:
+    shared_memory(const std::string &name, int size, bool create);
+
+    ~shared_memory();
+
+    void unlink();
+
+    void *ptr()
+    {
+        return ptr_;
+    }
+
+    int size() const
+    {
+        return size_;
+    }
+
+private:
+    std::string name_;
+
+    int size_;
+
+    int fd_;
+
+    void *ptr_;
+
+};
+
+class semaphore
+: public uncopyable
+{
+public:
+    semaphore(const std::string &name, int value = -1);
+
+    ~semaphore();
+
+    void unlink();
+
+    bool acquire(int timeout = -1);
+
+    bool try_acquire()
+    {
+        return acquire(0);
+    }
+
+    void release();
+
+private:
+    std::string name_;
+
+    sem_t *sem_;
+};
+
+}   // namespace cppev
+
+#endif  // ipc.h
---
+++ src/lib/ipc.cc
@@ -0,0 +1,112 @@
+#include "cppev/ipc.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+namespace cppev
+{
+
+shared_memory::shared_memory(const std::string &name, int size, bool create)
+: name_(name), size_(size)
+{
+    int flag = O_RDWR;
+    if (create)
+    {
+        flag |= O_CREAT | O_EXCL;
+    }
+    fd_ = shm_open(name_.c_str(), flag, 0777);
+    if (fd_ < 0)
+    {
+        throw_system_error("shm_open error");
+    }
+    if (create)
+    {
+        ftruncate(fd_, size_);
+    }
+    ptr_ = mmap(nullptr, size_, PROT_READ | PROT_WRITE, MAP_SHARED, fd_, 0);
+    if (ptr_ == MAP_FAILED)
+    {
+        throw_system_error("mmap error");
+    }
+}
+
+shared_memory::~shared_memory()
+{
+    close(fd_);
+}
+
+void shared_memory::unlink()
+{
+    if (shm_unlink(name_.c_str()) == -1)
+    {
+        throw_system_error("shm_unlink error");
+    }
+}
+
+
+
+semaphore::semaphore(const std::string &name, int value)
+: name_(name)
+{
+    if (value != -1)
+    {
+        sem_ = sem_open(name_.c_str(), O_CREAT | O_EXCL, 0777, value);
+    }
+    else
+    {
+        sem_ = sem_open(name_.c_str(), 0);
+    }
+    if (sem_ == SEM_FAILED)
+    {
+        throw_system_error("sem_open error");
+    }
+}
+
+semaphore::~semaphore()
+{
+    if (sem_close(sem_) == -1)
+    {
+        throw_system_error("sem_close error");
+    }
+}
+
+void semaphore::unlink()
+{
+    if (sem_unlink(name_.c_str()) == -1)
+    {
+        throw_system_error("sem_unlink error");
+    }
+}
+
+bool semaphore::acquire(int timeout)
+{
+    int ret;
+    if (timeout == -1)
+    {
+        ret = sem_wait(sem_);
+    }
+    else if (timeout == 0)
+    {
+        ret = sem_trywait(sem_);
+    }
+    if (ret == -1)
+    {
+        if (errno == EAGAIN || errno == EINTR)
+        {
+            return false;
+        }
+        std::string str = timeout == -1 ? "sem_wait" : "sem_trywait";
+        throw_system_error(str.append(" error"));
+    }
+    return true;
+}
+
+void semaphore::release()
+{
+    if (sem_post(sem_) == -1)
+    {
+        throw_system_error("sem_post error");
+    }
+}
+
+}   // namespace cppev
