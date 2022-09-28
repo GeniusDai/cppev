e1ada7b8c31fcee411358fd1134e4ca01b0fbb21
GeniusDai <1972789815@qq.com>
2022-09-28 23:49:42 +0800

    minor fix

--- src/include/cppev/subprocess.h
+++ src/include/cppev/subprocess.h
@@ -92,7 +92,7 @@ void popen::wait(const std::chrono::duration<Rep, Period> &interval)
     while (!poll())
     {
         communicate();
-        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        std::this_thread::sleep_for(interval);
     }
     communicate();
 }
