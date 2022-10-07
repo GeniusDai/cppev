d1be7ceae609f374df5a673dbea4f4eb03a8b922
GeniusDai <1972789815@qq.com>
2022-10-08 00:53:48 +0800

    update unittest of shm and lock

--- unittest/test_ipc.cc
+++ unittest/test_ipc.cc
@@ -1,8 +1,9 @@
-#include <gtest/gtest.h>
-#include "cppev/ipc.h"
 #include <thread>
 #include <mutex>
 #include <condition_variable>
+#include <gtest/gtest.h>
+#include "cppev/ipc.h"
+#include "cppev/lock.h"
 
 namespace cppev
 {
@@ -18,7 +19,20 @@ protected:
     {}
 };
 
-TEST_F(TestIpc, test_shm)
+struct TestStructBase
+{
+    TestStructBase()
+    {
+        std::cout << "constructor" << std::endl;
+    }
+
+    ~TestStructBase()
+    {
+        std::cout << "destructor" << std::endl;
+    }
+};
+
+TEST_F(TestIpc, test_shm_by_thread)
 {
     std::string shm_name{"/cppev_test_shm"};
     int shm_size = 128;
@@ -29,12 +43,10 @@ TEST_F(TestIpc, test_shm)
     bool ready = false;
 
     std::thread thr_writer([&]() {
-        {
-            std::unique_lock<std::mutex> lg(lock);
-            shared_memory shm(shm_name, shm_size, true);
-            memcpy(shm.ptr(), str.c_str(), str.size());
-            ready = true;
-        }
+        std::unique_lock<std::mutex> lg(lock);
+        shared_memory shm(shm_name, shm_size, true);
+        memcpy(shm.ptr(), str.c_str(), str.size());
+        ready = true;
         cv.notify_one();
     });
 
@@ -54,7 +66,7 @@ TEST_F(TestIpc, test_shm)
     thr_reader.join();
 }
 
-TEST_F(TestIpc, test_sem)
+TEST_F(TestIpc, test_sem_by_thread)
 {
     std::string sem_name{"/cppev_test_sem"};
     int sem_value = 3;
@@ -74,8 +86,8 @@ TEST_F(TestIpc, test_sem)
             std::unique_lock<std::mutex> lg(lock);
             psem = std::unique_ptr<semaphore>(new semaphore(sem_name, sem_value));
             ready = true;
+            cv.notify_one();
         }
-        cv.notify_one();
         std::this_thread::sleep_for(TIME_ACCURACY(delay));
         magic_num = MAGIC;
         psem->release();
@@ -116,7 +128,7 @@ TEST_F(TestIpc, test_sem)
     user.join();
 }
 
-TEST_F(TestIpc, test_by_fork)
+TEST_F(TestIpc, test_shm_sem_by_fork)
 {
     std::string name{"/cppev_test_name"};
     shared_memory shm(name, 128, true);
@@ -153,6 +165,126 @@ TEST_F(TestIpc, test_by_fork)
         int ret = -1;
         waitpid(pid, &ret, 0);
         EXPECT_EQ(ret, 0);
+    }
+}
+
+TEST_F(TestIpc, test_shm_rwlock_by_fork)
+{
+    struct TestStruct : public TestStructBase
+    {
+        TestStruct(int var1, double var2, std::string str) : var1(var1), var2(var2), str(str) {}
+
+        pshared_rwlock lock;
+        int var1;
+        double var2;
+        std::string str;
+    };
+
+    std::string name = "/cppev_test_shm_lock";
+
+    shared_memory shm(name, sizeof(TestStruct), true);
+    TestStruct *f_ptr = shm.construct<TestStruct, int, double, std::string>(0, 6.6, std::string("hi"));
+    EXPECT_EQ(reinterpret_cast<void *>(f_ptr), shm.ptr() );
+    EXPECT_TRUE(f_ptr->lock.try_wrlock());
+    f_ptr->lock.unlock();
+
+    pid_t pid = fork();
+    if (pid < 0)
+    {
+        throw_system_error("fork error");
+    }
+    if (pid == 0)
+    {
+        shared_memory sub_shm(name, sizeof(TestStruct), false);
+        TestStruct *c_ptr = reinterpret_cast<TestStruct *>(sub_shm.ptr());
+
+        EXPECT_TRUE(c_ptr->lock.try_wrlock());
+        c_ptr->var1 = 1;
+
+        // waiting for father assert
+        while (c_ptr->var1 != 0);
+
+        c_ptr->lock.unlock();
+
+        _exit(0);
+    }
+    else
+    {
+        // waiting for child wrlock
+        while (f_ptr->var1 != 1);
+
+        EXPECT_FALSE(f_ptr->lock.try_rdlock());
+        EXPECT_FALSE(f_ptr->lock.try_wrlock());
+
+        f_ptr->var1 = 0;
+
+        int ret = -1;
+        waitpid(pid, &ret, 0);
+        EXPECT_EQ(ret, 0);
+
+        f_ptr->~TestStruct();
+        shm.unlink();
+    }
+}
+
+TEST_F(TestIpc, test_shm_lock_cond_by_fork)
+{
+    struct TestStruct : public TestStructBase
+    {
+        TestStruct() : var(0), ready(false) {}
+
+        pshared_lock lock;
+        pshared_cond cond;
+        int var;
+        bool ready;
+    };
+
+    std::string name = "/cppev_test_shm_lock";
+
+    shared_memory shm(name, sizeof(TestStruct), true);
+    TestStruct *f_ptr = shm.construct<TestStruct>();
+    EXPECT_EQ(f_ptr, reinterpret_cast<TestStruct *>(shm.ptr()));
+    int num = 100;
+
+    pid_t pid = fork();
+    if (pid < 0)
+    {
+        throw_system_error("fork error");
+    }
+    else if (pid == 0)
+    {
+        shared_memory sub_shm(name, sizeof(TestStruct), false);
+        TestStruct *c_ptr = reinterpret_cast<TestStruct *>(sub_shm.ptr());
+        EXPECT_NE(shm.ptr(), sub_shm.ptr());
+        EXPECT_NE(f_ptr, c_ptr);
+        for (int i = 0; i < num; ++i)
+        {
+            while (!c_ptr->ready);  // fence
+            std::unique_lock<pshared_lock> lock(c_ptr->lock);
+            c_ptr->var = i;
+            c_ptr->ready = false;
+            c_ptr->cond.notify_one();
+        }
+        _exit(0);
+    }
+    else
+    {
+        int sum = 0;
+        for (int i = 0; i < num; ++i)
+        {
+            std::unique_lock<pshared_lock> lock(f_ptr->lock);
+            f_ptr->ready = true;
+            f_ptr->cond.wait(lock);
+            sum += f_ptr->var;
+        }
+        EXPECT_EQ(sum, (0 + num - 1) * num / 2);
+
+        int ret = -1;
+        waitpid(pid, &ret, 0);
+        EXPECT_EQ(ret, 0);
+
+        f_ptr->~TestStruct();
+        shm.unlink();
     }
 }
 
--- unittest/test_lock.cc
+++ unittest/test_lock.cc
@@ -13,8 +13,7 @@ class TestLock
 protected:
     void SetUp() override
     {
-        ready1_ = false;
-        ready2_ = false;
+        ready_ = false;
     }
 
     void TearDown() override
@@ -22,43 +21,38 @@ protected:
 
     std::mutex lock_;
 
-    std::condition_variable cond1_;
+    std::condition_variable cond_;
 
-    std::condition_variable cond2_;
-
-    bool ready1_;
-
-    bool ready2_;
+    bool ready_;
 };
 
 TEST_F(TestLock, test_rwlock_rdlocked)
 {
     pshared_rwlock rwlck;
-    auto func = [this, &rwlck]() -> void
+
+    // sub-thread
+    auto func = [this, &rwlck]()
     {
         std::unique_lock<std::mutex> lock(this->lock_);
-        this->ready1_ = true;
+        this->ready_ = true;
         rwlck.rdlock();
-        this->cond1_.notify_one();
-        this->cond2_.wait(lock,
-            [this]() -> bool
-            {
-                return this->ready2_;
-            }
-        );
+        this->cond_.notify_one();
+        this->cond_.wait(lock);
+        rwlck.unlock();
+        ASSERT_TRUE(rwlck.try_wrlock());
         rwlck.unlock();
     };
-
     std::thread thr(func);
 
+    // main-thread
     {
         std::unique_lock<std::mutex> lock(lock_);
-        if (!ready1_)
+        if (!ready_)
         {
-            cond1_.wait(lock,
+            cond_.wait(lock,
                 [this] () -> bool
                 {
-                    return this->ready1_;
+                    return this->ready_;
                 }
             );
         }
@@ -66,9 +60,7 @@ TEST_F(TestLock, test_rwlock_rdlocked)
         ASSERT_TRUE(rwlck.try_rdlock());
         rwlck.unlock();
         ASSERT_FALSE(rwlck.try_wrlock());
-
-        ready2_ = true;
-        cond2_.notify_one();
+        cond_.notify_one();
     }
     thr.join();
 }
@@ -76,204 +68,68 @@ TEST_F(TestLock, test_rwlock_rdlocked)
 TEST_F(TestLock, test_rwlock_wrlocked)
 {
     pshared_rwlock rwlck;
-    auto func = [this, &rwlck]() -> void
+
+    // sub-thread
+    auto func = [this, &rwlck]()
     {
         std::unique_lock<std::mutex> lock(this->lock_);
-        this->ready1_ = true;
+        this->ready_ = true;
         rwlck.wrlock();
-        this->cond1_.notify_one();
-        this->cond2_.wait(lock,
-            [this]() -> bool
-            {
-                return this->ready2_;
-            }
-        );
+        this->cond_.notify_one();
+        this->cond_.wait(lock);
         rwlck.unlock();
     };
-
     std::thread thr(func);
 
+    // main-thread
     {
         std::unique_lock<std::mutex> lock(lock_);
-        if (!ready1_)
+        if (!ready_)
         {
-            cond1_.wait(lock,
+            cond_.wait(lock,
                 [this] () -> bool
                 {
-                    return this->ready1_;
+                    return this->ready_;
                 }
             );
         }
         ASSERT_FALSE(rwlck.try_rdlock());
         ASSERT_FALSE(rwlck.try_wrlock());
-        ready2_ = true;
-        cond2_.notify_one();
+        cond_.notify_one();
     }
     thr.join();
-}
-
-TEST_F(TestLock, test_shm_rwlock)
-{
-    struct TestStruct
-    {
-        TestStruct(int var1, double var2, std::string str)
-        : var1(var1), var2(var2), str(str)
-        {
-            std::cout << "constructor" << std::endl;
-        }
-        ~TestStruct()
-        {
-            std::cout << "destructor" << std::endl;
-        }
-        pshared_rwlock lock;
-        int var1;
-        double var2;
-        std::string str;
-    };
-
-    std::string name = "/cppev_test_shm_lock";
-
-    shared_memory shm(name, sizeof(TestStruct), true);
-    TestStruct *f_ptr = shm.construct<TestStruct, int, double, std::string>(0, 6.6, std::string("hi"));
-    EXPECT_EQ(reinterpret_cast<void *>(f_ptr), shm.ptr() );
-    EXPECT_TRUE(f_ptr->lock.try_wrlock());
-    f_ptr->lock.unlock();
-
-    pid_t pid = fork();
-    if (pid < 0)
-    {
-        throw_system_error("fork error");
-    }
-    if (pid == 0)
-    {
-        shared_memory sub_shm(name, sizeof(TestStruct), false);
-        TestStruct *c_ptr = reinterpret_cast<TestStruct *>(sub_shm.ptr());
-
-        EXPECT_TRUE(c_ptr->lock.try_wrlock());
-        c_ptr->var1 = 1;
-
-        // waiting for father assert
-        while (c_ptr->var1 != 0);
-
-        c_ptr->lock.unlock();
-
-        _exit(0);
-    }
-    else
-    {
-        // waiting for child wrlock
-        while (f_ptr->var1 != 1);
-
-        EXPECT_FALSE(f_ptr->lock.try_rdlock());
-        EXPECT_FALSE(f_ptr->lock.try_wrlock());
-
-        f_ptr->var1 = 0;
-
-        int ret = -1;
-        waitpid(pid, &ret, 0);
-        EXPECT_EQ(ret, 0);
-
-        f_ptr->~TestStruct();
-        shm.unlink();
-    }
-}
-
-TEST_F(TestLock, test_shm_lock_cond)
-{
-    struct TestStruct
-    {
-        TestStruct() : var(0), ready(false) {}
-        pshared_lock lock;
-        pshared_cond cond;
-        int var;
-        bool ready;
-    };
-
-    std::string name = "/cppev_test_shm_lock";
-
-    shared_memory shm(name, sizeof(TestStruct), true);
-    TestStruct *f_ptr = shm.construct<TestStruct>();
-    EXPECT_EQ(f_ptr, reinterpret_cast<TestStruct *>(shm.ptr()));
-    int num = 100;
-
-    pid_t pid = fork();
-    if (pid < 0)
-    {
-        throw_system_error("fork error");
-    }
-    else if (pid == 0)
-    {
-        shared_memory sub_shm(name, sizeof(TestStruct), false);
-        TestStruct *c_ptr = reinterpret_cast<TestStruct *>(sub_shm.ptr());
-        EXPECT_NE(shm.ptr(), sub_shm.ptr());
-        EXPECT_NE(f_ptr, c_ptr);
-        for (int i = 0; i < num; ++i)
-        {
-            while (!c_ptr->ready);  // fence
-            std::unique_lock<pshared_lock> lock(c_ptr->lock);
-            c_ptr->var = i;
-            c_ptr->ready = false;
-            c_ptr->cond.notify_one();
-        }
-        _exit(0);
-    }
-    else
-    {
-        int sum = 0;
-        for (int i = 0; i < num; ++i)
-        {
-            std::unique_lock<pshared_lock> lock(f_ptr->lock);
-            f_ptr->ready = true;
-            f_ptr->cond.wait(lock);
-            sum += f_ptr->var;
-        }
-        EXPECT_EQ(sum, (0 + num - 1) * num / 2);
-
-        int ret = -1;
-        waitpid(pid, &ret, 0);
-        EXPECT_EQ(ret, 0);
-
-        f_ptr->~TestStruct();
-        shm.unlink();
-    }
 }
 
 TEST_F(TestLock, test_spinlock)
 {
     spinlock splck;
-    splck.lock();
-    splck.unlock();
-    auto func = [&]() -> void
+
+    // sub-thread
+    auto func = [this, &splck]() -> void
     {
-        std::unique_lock<std::mutex> lk(lock_);
-        ready1_ = true;
+        std::unique_lock<std::mutex> lock(lock_);
+        this->ready_ = true;
         ASSERT_TRUE(splck.trylock());
-        cond1_.notify_one();
-        cond2_.wait(lk,
-            [&]() -> bool
-            {
-                return ready2_;
-            }
-        );
+        this->cond_.notify_one();
+        this->cond_.wait(lock);
         splck.unlock();
     };
-
     std::thread thr(func);
 
+    // main-thread
     {
         std::unique_lock<std::mutex> lk(lock_);
-        if (!ready1_)
+        if (!ready_)
         {
-            cond1_.wait(lk,
-                [&] () -> bool
+            cond_.wait(lk,
+                [this] () -> bool
                 {
-                    return ready1_;
+                    return this->ready_;
                 }
             );
         }
         ASSERT_FALSE(splck.trylock());
-        ready2_ = true;
-        cond2_.notify_one();
+        cond_.notify_one();
     }
     thr.join();
 }
