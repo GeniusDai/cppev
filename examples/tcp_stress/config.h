e6f72386ce33b882fdd2af9c67de8ef507c3ada5
GeniusDai <1972789815@qq.com>
2025-02-24 20:53:55 +0800

    minor update example

--- examples/tcp_stress/config.h
+++ examples/tcp_stress/config.h
@@ -8,15 +8,17 @@
 const int IPV4_CONCURRENCY = 8000;
 const int IPV6_CONCURRENCY = 8000;
 const int UNIX_CONCURRENCY = 2000;
+const int SERVER_WORKER_NUM = 32;
+const int CLIENT_WORKER_NUM = 32;
+const int CONTOR_NUM = 3;
 #else
 const int IPV4_CONCURRENCY = 100;
 const int IPV6_CONCURRENCY = 100;
 const int UNIX_CONCURRENCY = 100;
+const int SERVER_WORKER_NUM = 3;
+const int CLIENT_WORKER_NUM = 3;
+const int CONTOR_NUM = 1;
 #endif
-
-const int SERVER_WORKER_NUM = 32;
-const int CLIENT_WORKER_NUM = 32;
-const int CONTOR_NUM = 3;
 
 const int   IPV4_PORT = 8884;
 const int   IPV6_PORT = 8886;
