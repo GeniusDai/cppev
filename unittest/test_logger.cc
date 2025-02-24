1506f2abfbb8d1b71e0dddb4dec334112f616e76
GeniusDai <1972789815@qq.com>
2025-02-24 21:26:35 +0800

    add logger sample file

---
+++ unittest/test_logger.cc
@@ -0,0 +1,32 @@
+#include <filesystem>
+#include "cppev/logger.h"
+
+int main(int argc, char **argv) {
+    std::string log_file_path = std::string(std::filesystem::path(argv[0]).parent_path())
+        + "/logger_output_file.log";
+
+    // Configure logger
+    std::ofstream log_file(log_file_path);
+    cppev::logger::get_instance().add_output_stream(log_file);
+    cppev::logger::get_instance().set_log_level(cppev::log_level::debug);
+
+    // Stream-style logging
+    LOG_DEBUG << "LOG_DEBUG Message";
+    LOG_INFO << "LOG_INFO Message";
+    LOG_WARNING << "LOG_WARNING Message";
+    LOG_ERROR << std::string("LOG_ERROR Message");
+
+    auto logging_task = []() {
+        // Formatted logging
+        LOG_DEBUG_FMT("LOG_DEBUG_FMT Message : %s %d", "thread", std::this_thread::get_id());
+        LOG_INFO_FMT("LOG_INFO_FMT Message : %s %d", "thread", std::this_thread::get_id());
+        LOG_WARNING_FMT("LOG_WARNING_FMT Message : %s %d", "thread", std::this_thread::get_id());
+        LOG_ERROR_FMT("LOG_ERROR_FMT Message : %s %d", std::string("thread").c_str(), std::this_thread::get_id());
+    };
+
+    std::thread thr(logging_task);
+
+    thr.join();
+
+    return 0;
+}
