161de0e1df17675788e63310c6798876293aa0cf
GeniusDai <1972789815@qq.com>
2025-02-26 14:04:19 +0000

    update test logger

--- unittest/test_logger.cc
+++ unittest/test_logger.cc
@@ -8,25 +8,36 @@ int main(int argc, char **argv) {
     // Configure logger
     std::ofstream log_file(log_file_path);
     cppev::logger::get_instance().add_output_stream(log_file);
-    cppev::logger::get_instance().set_log_level(cppev::log_level::debug);
 
-    // Stream-style logging
-    LOG_DEBUG << "LOG_DEBUG Message";
-    LOG_INFO << "LOG_INFO Message";
-    LOG_WARNING << "LOG_WARNING Message";
-    LOG_ERROR << "LOG_ERROR Message";
-
-    auto logging_task = []() {
-        // Formatted logging
-        LOG_DEBUG_FMT("LOG_DEBUG_FMT Message : %s %d", "count", 1);
-        LOG_INFO_FMT("LOG_INFO_FMT Message : %s %d", "count", 2);
-        LOG_WARNING_FMT("LOG_WARNING_FMT Message : %s %d", "count", 3);
-        LOG_ERROR_FMT("LOG_ERROR_FMT Message : %s %d", "count", 4);
+    std::vector<cppev::log_level> levels = {
+        cppev::log_level::debug,
+        cppev::log_level::info,
+        cppev::log_level::warning,
+        cppev::log_level::error,
     };
 
-    std::thread thr(logging_task);
+    for (auto level : levels)
+    {
+        cppev::logger::get_instance().set_log_level(level);
 
-    thr.join();
+        // Stream-style logging
+        LOG_DEBUG << "LOG_DEBUG Message";
+        LOG_INFO << "LOG_INFO Message";
+        LOG_WARNING << "LOG_WARNING Message";
+        LOG_ERROR << "LOG_ERROR Message";
+
+        auto logging_task = []() {
+            // Formatted logging
+            LOG_DEBUG_FMT("LOG_DEBUG_FMT Message : %s %d", "count", 1);
+            LOG_INFO_FMT("LOG_INFO_FMT Message : %s %d", "count", 2);
+            LOG_WARNING_FMT("LOG_WARNING_FMT Message : %s %d", "count", 3);
+            LOG_ERROR_FMT("LOG_ERROR_FMT Message : %s %d", "count", 4);
+        };
+        std::thread thr(logging_task);
+        thr.join();
+
+        std::cout << std::endl;
+    }
 
     return 0;
 }
